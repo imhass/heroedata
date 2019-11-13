6177edccfada710a57457bb5e4b792ff2ead0a42
imad <it@hassani.de>
2019-11-13 11:28:50 +0100

    Initial Commit

---
+++ src/main/java/com/example/heroedata/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.heroedata;
+
+import com.microsoft.azure.spring.data.cosmosdb.repository.ReactiveCosmosRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface UserRepository extends ReactiveCosmosRepository<User, String> {
+    Flux<User> findByFirstName(String firstName);
+}
---
+++ src/test/java/com/example/heroedata/HeroedataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heroedata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroedataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
